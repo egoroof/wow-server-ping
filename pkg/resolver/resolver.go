5321acb76ef00790651f92b96b013ad147cf5930
egoroof <egoroof@protonmail.com>
2026-08-20 12:25:34 +0800

    sort HostIps

--- pkg/resolver/resolver.go
+++ pkg/resolver/resolver.go
@@ -3,14 +3,22 @@ package resolver
 import (
 	"context"
 	"net"
+	"slices"
+	"strings"
 	"time"
 )
 
 var resolver = &net.Resolver{}
 
-func LookupHost(host string, timeout time.Duration) (ips []string, err error) {
+func LookupHost(host string, timeout time.Duration) ([]string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	return resolver.LookupHost(ctx, host)
+	ips, err := resolver.LookupHost(ctx, host)
+
+	slices.SortFunc(ips, func(a, b string) int {
+		return strings.Compare(a, b)
+	})
+
+	return ips, err
 }
